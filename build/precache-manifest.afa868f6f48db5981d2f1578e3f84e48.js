5d07aed10ed48fb61f13290c7f6972512002baf2
Jordan Nishina <jordannishina2021@u.northwestern.edu>
2019-11-01 19:18:49 -0500

    moved files out of quick_doc

---
+++ build/precache-manifest.afa868f6f48db5981d2f1578e3f84e48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8134b785a8e597d8d2ca01df24db995b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8dc52f6c63a9ca63d7c9",
+    "url": "/static/css/2.58bd2952.chunk.css"
+  },
+  {
+    "revision": "a540e1e933067966addf",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "8dc52f6c63a9ca63d7c9",
+    "url": "/static/js/2.f0f337eb.chunk.js"
+  },
+  {
+    "revision": "a540e1e933067966addf",
+    "url": "/static/js/main.5dd90988.chunk.js"
+  },
+  {
+    "revision": "24b07840cde4cdf7f3aa",
+    "url": "/static/js/runtime-main.effd4e97.js"
+  }
+]);
